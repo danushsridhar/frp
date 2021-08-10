00793c0f670fc52bf49041115ff3ee0c403d12f6
danushsridhar <sridhanush@gmail.com>
2021-08-11 02:09:49 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+import cv2
+
+#load some pre-trained data on face frontals from opencv (haar casacade algortihm)
+trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+
+#Choose an image
+img = cv2.imread('img2.jpg')
+
+#convert the chosen image to grayscale
+gray_img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+#detect faces
+face_coordinates = trained_face_data.detectMultiScale(gray_img)
+
+print(face_coordinates)
+
+#display image
+cv2.imshow('FD',gray_img)
+cv2.waitKey()
+
+print("End of program")
+
