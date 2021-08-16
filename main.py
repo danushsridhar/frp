4e814591b96ad2ead80a1e584284acb40a05be00
danushsridhar <sridhanush@gmail.com>
2021-08-16 20:19:16 +0530

    testing git

--- main.py
+++ main.py
@@ -4,19 +4,37 @@ import cv2
 trained_face_data = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 #Choose an image
-img = cv2.imread('img2.jpg')
+#img = cv2.imread('img8.jpg')
 
-#convert the chosen image to grayscale
-gray_img = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+#to capture video from webcam
+webcam = cv2.VideoCapture(0)
 
-#detect faces
-face_coordinates = trained_face_data.detectMultiScale(gray_img)
+#iterate over frames
+while True:
 
-print(face_coordinates)
+    #### read the current frame
+    successful_frame_read, frame= webcam.read()
 
-#display image
-cv2.imshow('FD',gray_img)
-cv2.waitKey()
+
+    #convert the chosen image to grayscale
+    gray_img = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+
+    #detect faces
+    face_coordinates = trained_face_data.detectMultiScale(gray_img)
+
+    #draw rectangles around the faces
+    for(x,y,w,h) in face_coordinates:
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),1)
+
+
+    print(face_coordinates)
+
+    #display image with faces selected
+    cv2.imshow('FD',frame)
+    key = cv2.waitKey(1)
+
+    if key==81 or key==113:
+        break;
 
 print("End of program")
 
